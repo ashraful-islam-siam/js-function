24dd548c5f90c9b3b794fc1e213f9dfddf09b27b
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-09-06 10:28:41 +0600

    Started practicing js function code

---
+++ function.js
@@ -0,0 +1,30 @@
+const price = 43;
+
+// function declare
+function fanOffKor() {
+  console.log("bosa theke uthe dara");
+  console.log("walk towards the switch");
+  console.log("click the switch to off of the Fan");
+}
+
+// function call/invoke
+fanOffKor();
+
+/**
+ * function name() {
+ *    // code
+ * }
+ */
+function brushTeeth() {
+  console.log("pick up the bruch");
+  console.log("add paste");
+  console.log("2 min goso and goso and fena felo");
+}
+
+// call the function
+brushTeeth();
+console.log("------------");
+console.log("------------");
+console.log("------------");
+console.log("tom tom ta to");
+brushTeeth();
